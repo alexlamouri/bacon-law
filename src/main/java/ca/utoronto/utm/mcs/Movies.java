3c56b38d356b748cdd7281da63b62f1193268329
Viju Hiremath <Viju@Viju.local>
2020-02-04 20:48:28 -0500

    changed imports

--- src/main/java/ca/utoronto/utm/mcs/Movies.java
+++ src/main/java/ca/utoronto/utm/mcs/Movies.java
@@ -2,7 +2,11 @@ package ca.utoronto.utm.mcs;
 
 import java.io.IOException;
 
-import javax.xml.ws.spi.http.HttpExchange;
+import org.neo4j.driver.v1.*;
+
+// import static org.neo4j.driver.Values.parameters;
+
+// import javax.xml.ws.spi.http.HttpExchange;
 
 import org.json.JSONException;
 
@@ -10,7 +14,7 @@ import org.json.JSONException;
 //import java.net.InetSocketAddress;
 //import com.sun.net.httpserver.HttpServer;
 
-public class Movies {
+/* public class Movies {
 
     private static Memory memory;
 
@@ -23,7 +27,7 @@ public class Movies {
      * 
      * public Movies(String movieID, String name) { this.movieID = movieID;
      * this.name = name; }
-     */
+     
 
     public void handle(HttpExchange r) {
         try {
@@ -70,4 +74,59 @@ public class Movies {
 
     }
 
+} */
+
+public class Movies {
+    // Driver objects are thread-safe and are typically made available
+    // application-wide.
+    Driver driver;
+
+    public SmallExample(String uri, String user, String password)
+    {
+        driver = GraphDatabase.driver(uri, AuthTokens.basic(user, password));
+    }
+
+    private void addPerson(String name) {
+        // Sessions are lightweight and disposable connection wrappers.
+        try (Session session = driver.session()) {
+            // Wrapping a Cypher Query in a Managed Transaction provides atomicity
+            // and makes handling errors much easier.
+            // Use `session.writeTransaction` for writes and `session.readTransaction` for
+            // reading data.
+            // These methods are also able to handle connection problems and transient
+            // errors using an automatic retry mechanism.
+            session.writeTransaction(tx -> tx.run("MERGE (a:Person {name: $x})", parameters("x", name)));
+        }
+    }
+
+    private void printPeople(String initial) {
+        try (Session session = driver.session()) {
+            // A Managed Transaction transactions are a quick and easy way to wrap a Cypher
+            // Query.
+            // The `session.run` method will run the specified Query.
+            // This simpler method does not use any automatic retry mechanism.
+            Result result = session.run("MATCH (a:Person) WHERE a.name STARTS WITH $x RETURN a.name AS name",
+                    parameters("x", initial));
+            // Each Cypher execution returns a stream of records.
+            while (result.hasNext()) {
+                Record record = result.next();
+                // Values can be extracted from a record by index or name.
+                System.out.println(record.get("name").asString());
+            }
+        }
+    }
+
+    public void close() {
+        // Closing a driver immediately shuts down all open connections.
+        driver.close();
+    }
+
+    public static void main(String... args) {
+        SmallExample example = new SmallExample("bolt://localhost:7687", "neo4j", "password");
+        example.addPerson("Ada");
+        example.addPerson("Alice");
+        example.addPerson("Bob");
+        example.printPeople("A");
+        example.close();
+    }
 }
